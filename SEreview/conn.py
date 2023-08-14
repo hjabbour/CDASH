5150d46033731618c2136dab86c4d92da35d3562
hjabbour <haytham.jabbour@gmail.com>
2023-08-14 17:30:41 +0400

    created connection file

---
+++ SEreview/conn.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+def get_mongodb_connection():
+    #client = MongoClient('mongodb://root:password@10.229.166.36:27017')
+    #client = MongoClient('mongodb://root:password@192.168.2.158:27017')
+    client = MongoClient('mongodb://root:password@127.0.0.1:27017')
+
+    return client
+
