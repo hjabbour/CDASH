80f84e7c8c9dd6c2b7b1c0456a0367898227a85c
hjabbour <haytham.jabbour@gmail.com>
2023-08-17 10:21:06 +0000

    env Mongo url

--- SEreview/conn.py
+++ SEreview/conn.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
 
 def get_mongodb_connection():
-    #client = MongoClient('mongodb://root:password@10.229.166.36:27017')
-    client = MongoClient('mongodb://root:password@192.168.2.158:27017')
-    #client = MongoClient('mongodb://root:password@127.0.0.1:27017')
-
+    mongodb_url = os.environ.get('MONGODB_URL')
+    if mongodb_url:
+        client = MongoClient(mongodb_url)
+    else:
+        # Fallback to a hardcoded URL
+        #client = MongoClient('mongodb://root:password@10.229.166.36:27017')
+        client = MongoClient('mongodb://root:password@192.168.2.158:27017')
+        #client = MongoClient('mongodb://root:password@127.0.0.1:27017')
+        
     return client
-
