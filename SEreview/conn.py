6522ffbe38487ee1858ce7be7bded871a70c6b71
hjabbour <haytham.jabbour@gmail.com>
2023-08-17 07:35:13 +0000

    sqlite3

--- SEreview/conn.py
+++ SEreview/conn.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 def get_mongodb_connection():
     #client = MongoClient('mongodb://root:password@10.229.166.36:27017')
-    #client = MongoClient('mongodb://root:password@192.168.2.158:27017')
-    client = MongoClient('mongodb://root:password@127.0.0.1:27017')
+    client = MongoClient('mongodb://root:password@192.168.2.158:27017')
+    #client = MongoClient('mongodb://root:password@127.0.0.1:27017')
 
     return client
 
