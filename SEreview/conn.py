741cd6ebadd6e7e051252d033496dbb3f3ab5158
hjabbour <haytham.jabbour@gmail.com>
2023-09-11 06:29:38 +0000

    fixed stats and weekly review

--- SEreview/conn.py
+++ SEreview/conn.py
@@ -8,8 +8,8 @@ def get_mongodb_connection():
     else:
         # Fallback to a hardcoded URL
         #client = MongoClient('mongodb://root:password@10.229.166.36:27017')
-        client = MongoClient('mongodb://root:password@192.168.2.114:27017')
-        #client = MongoClient('mongodb://root:password@10.229.164.20:27017')
+        #client = MongoClient('mongodb://root:password@192.168.2.114:27017')
+        client = MongoClient('mongodb://root:password@10.229.166.167:27017')
         
         #client = MongoClient('mongodb://root:password@127.0.0.1:27017')
         
